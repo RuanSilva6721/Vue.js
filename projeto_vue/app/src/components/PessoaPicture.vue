dcbb6b5910216b671b0620fafd13a058096669f1
RuanFelipe <ruansilva6721@gmail.com>
2022-11-10 09:52:01 -0300

    usado emit

--- projeto_vue/app/src/components/PessoaPicture.vue
+++ projeto_vue/app/src/components/PessoaPicture.vue
@@ -2,17 +2,26 @@
     <div>
         <p>teste</p>
         <img class="imgTamanhp" :src="myavatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="mudarImagem" />
     </div>
 </template>
 
 
-<script>
+<script>import MudarImagem from "./MudarImagem.vue"
 export default {
     name: "PessoaPicture",
     data(){
         return{
         myavatar: "/img/cara.webp",
         descricao: "Ruan"
+        }
+    },
+    components: {
+        MudarImagem
+    },
+    methods: {
+        mudarImagem(){
+            this.myavatar = "/img/teste.jpg"
         }
     }
 
