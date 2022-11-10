1481e65a691cb905ea3d32935206e2fab75ecc10
RuanFelipe <ruansilva6721@gmail.com>
2022-11-10 08:28:01 -0300

    usando v-for

--- projeto_vue/app/src/components/PessoaInfo.vue
+++ projeto_vue/app/src/components/PessoaInfo.vue
@@ -10,7 +10,23 @@
             <button @click="showEmail">Mostrar e-mail</button>
         </div>
         <p v-show="mostrar_email">meu e-mail é teste@email.com</p>
+
+
+        <p>testando back-end</p>
+
+        <ul>
+            <li v-for="(tec, index) in backend_tech" v-bind:key="index">{{tec}}</li>
+    <p>a mais</p>
+        </ul>
+
+
+        <p>ultilizo as linguagem front-end</p>
+        <ul>
+            <li v-for="(tec, index) in front_tech" v-bind:key="index">{{tec.language}}</li>
+        </ul>
     </div>
+
+
 </template>
 <script>
 export default {
@@ -19,7 +35,13 @@ export default {
         return{
         esta_trabalhando: false,
         meu_link: 'https://google.com',
-        mostrar_email: true
+        mostrar_email: true,
+        backend_tech: ['Python', 'PHP', 'JavaScript'],
+        front_tech: [
+            {id: 1, language: 'HTML'}, 
+            {id: 2, language: 'CSS'},
+            {id: 3, language: 'VUE.JS'},
+        ]
         }
     },
     methods:{
